ead5930313df5d9edd2c3d569fc09fc3fac0c18e
Dan Jelf <dan.jelf@cerner.com>
2018-11-27 15:31:44 -0600

    Module 3

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use(express.static(path.join(__dirname, "public")));
-
+app.use(express.urlencoded({extended:true}));
 
 const accountData = fs.readFileSync(
   path.join(__dirname, 'json', 'accounts.json'), 'utf8'
@@ -19,6 +19,14 @@ const userData = fs.readFileSync(
   path.join(__dirname, 'json', 'users.json'), 'utf8'
 )
 const users = JSON.parse(userData);
+
+app.get("/payment", (req, res)=> {
+    res.render("payment", {account: accounts.credit});
+})
+
+app.get("/transfer", (req, res) => {
+  res.render("transfer");
+})
 
 app.get("/profile", (req, res) => {
   res.render("profile", {user: users[0]});
@@ -39,5 +47,25 @@ app.get("/credit", (req, res) => {
 app.get("/", (req, res) => {
   res.render("index", { title: "Account Summary", accounts: accounts });
 });
+
+app.post("/payment", (req, res)=>{
+  
+  accounts.credit.balance = parseInt(accounts.credit.balance) 
+    - parseInt(req.body.amount);
+  accounts.credit.available = parseInt(accounts.credit.available)
+    + parseInt(req.body.amount);
+  const accountsJSON = JSON.stringify(accounts);
+  fs.writeFileSync(path.join(__dirname, "json/accounts.json"), accountsJSON, 'utf8');
+  res.render("payment", {message: "Payment Successful", account: accounts.credit});
+})
+
+app.post("/transfer", (req, res) => {
+  accounts[req.body.from].balance = accounts[req.body.from].balance - parseInt(req.body.amount);
+  accounts[req.body.to].balance = accounts[req.body.to].balance + parseInt(req.body.amount);
+  const accountsJSON = JSON.stringify(accounts);
+  fs.writeFileSync(path.join(__dirname, "json/accounts.json"), accountsJSON, 'utf8');
+  res.render("transfer", { message:"Transfer Completed" });
+});
+
 
 app.listen(3000, () => console.log("PS Project Running on port 3000!"));
